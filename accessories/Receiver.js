1f14056aa5e955ff69d0ce3bd538539515ef6430
Adriaan <adriaan.bernstein@gmx.com>
2024-12-08 17:31:43 +0100

    Rewrite of plugin for TX-8050: removed unused features, adapted for specific use-case

--- accessories/Receiver.js
+++ accessories/Receiver.js
@@ -1,98 +1,93 @@
-let Characteristic, Service
 const stateManager = require('../lib/stateManager')
+
+let Characteristic, Service
 
 class AUDIO_RECEIVER {
 	constructor(avr, platform, config) {
-
 		Service = platform.api.hap.Service
 		Characteristic = platform.api.hap.Characteristic
-		
+
 		this.storage = platform.storage
 		this.avr = avr
 		this.log = platform.log
 		this.api = platform.api
-		this.avrId = config.id
-		this.id = `${config.id}_${config.zone}`
-		this.ip = config["ip"] //AJB
-		this.zone = config.zone
-		this.name = config.name || `${config.avrName} ${this.zone.charAt(0).toUpperCase() + this.zone.slice(1)}`
-		this.serial = this.id
-		this.model = config.model || 'unknown'
-		this.manufacturer = this.model.toLowerCase().includes('vsx') ? 'Pioneer' : 'Onkyo'
-		this.displayName = this.name
-		this.inputs = config.inputs
-		this.maxVolume = config.maxVolume || 100
-		this.volume = config.volume
-		if (!this.volume.name)
-			this.volume.name = `${this.name} Volume`
+		this.config = config
 
+		this.avrId = 'mainZone'
+		this.id = 'mainZone'
+		this.name = this.config.name || 'Onkyo TX-8050'
+		this.model = this.config.info.modelName
+		this.manufacturer = 'Onkyo'
+		this.displayName = this.name
+		this.inputs = require('../lib/inputs').main.map((input, i) => ({identifier: i, name: input, key: input, hidden: 0}))
+		this.maxVolume = this.config.maxVolume || 75
+		this.volumeAccessory = this.config.volumeAccessory || 'bulb'
 
 		this.cachedStates = platform.cachedStates
-		this.cachedDevices = platform.cachedDevices
-		this.cachedDevice = this.cachedDevices.find(cachedDevice => cachedDevice.id === this.avrId)
-		
 		this.processing = false
-
 		this.UUID = this.api.hap.uuid.generate(this.id)
-		this.log.easyDebug(`Creating New AUDIO RECEIVER Accessory: "${this.name}"`)
+
+		// Bind getState function so we can call this.getState() easily
+		this.getState = stateManager.getState.bind(this)
+
+		this.log.easyDebug(`Creating AUDIO RECEIVER: "${this.name}"`)
+
 		this.accessory = new this.api.platformAccessory(this.name, this.UUID, this.api.hap.Accessory.Categories.AUDIO_RECEIVER)
 
-
 		let informationService = this.accessory.getService(Service.AccessoryInformation)
-
 		if (!informationService)
 			informationService = this.accessory.addService(Service.AccessoryInformation)
 
 		informationService
 			.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
 			.setCharacteristic(Characteristic.Model, this.model)
-			.setCharacteristic(Characteristic.SerialNumber, this.serial)
+			.setCharacteristic(Characteristic.SerialNumber, 'SingleZone-1')
 
-		
 		this.setServices()
 			.then(() => {
-				this.api.publishExternalAccessories(platform.PLUGIN_NAME, [this.accessory])
-				setInterval(this.updateState.bind(this), platform.statePollingInterval * 1000)
+				this.api.publishExternalAccessories('homebridge-onkyo-tx8050', [this.accessory])
+				// Polling interval
+				setInterval(() => {
+					// Skip polling if still powering on
+					if (this.isPoweringOn) {
+						this.log.easyDebug(`Skipping poll, still in powerOnDelay period`)
+						return
+					}
+					this.updateState()
+				}, this.config.statePollingInterval * 1000)
 			})
 			.catch(err => {
 				this.log('ERROR setting services')
 				this.log(err)
-			}) 
-
-    
+			})
 	}
 
 	async setServices() {
-		this.state = await stateManager.getState.bind(this)()
+		this.state = await this.getState()
 
 		this.tvService = this.accessory.addService(Service.Television, this.name)
 
 		this.tvService.getCharacteristic(Characteristic.ConfiguredName)
 			.on('set', (name, callback) => {
-				this.log.easyDebug(`Setting new ConfiguredName: from ${this.name} to ${name}`)
-				this.cachedDevice[this.zone].name = name
-				this.storage.setItem('cachedDevices', this.cachedDevices)
+				this.log.easyDebug(`ConfiguredName from ${this.name} to ${name}`)
+				this.name = name
 				callback()
 			}).updateValue(this.name)
 
 		this.tvService.getCharacteristic(Characteristic.Active)
 			.on('set', stateManager.set.Active.bind(this))
 			.updateValue(this.state.power)
-			
 
 		this.tvService.getCharacteristic(Characteristic.ActiveIdentifier)
 			.on('set', stateManager.set.ActiveIdentifier.bind(this))
 			.updateValue(this.state.source)
 
-
 		this.tvService.getCharacteristic(Characteristic.RemoteKey)
 			.on('set', stateManager.set.RemoteKey.bind(this))
 
-		this.tvService.setCharacteristic(Characteristic.SleepDiscoveryMode, 1)
+		this.tvService.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE)
 
-		// Set inputs
 		this.inputs.forEach(input => {
-			this.log.easyDebug(`${this.name} - Adding INPUT: ${input.name}`)
 			const inputUUID = this.api.hap.uuid.generate(this.id + input.key)
 			const inputService = this.accessory.addService(Service.InputSource, input.name, inputUUID)
 				.setCharacteristic(Characteristic.Identifier, input.identifier)
@@ -103,19 +98,17 @@ class AUDIO_RECEIVER {
 
 			inputService.getCharacteristic(Characteristic.TargetVisibilityState)
 				.on('set', (hidden, callback) => {
-					this.log.easyDebug(`${this.name} - Setting new input Visibility State to ${hidden ? 'HIDDEN' : 'VISIBLE'}`)
+					this.log.easyDebug(`Input ${input.name} now ${hidden ? 'HIDDEN' : 'VISIBLE'}`)
 					inputService.getCharacteristic(Characteristic.CurrentVisibilityState).updateValue(hidden)
 					input.hidden = hidden
-					this.storage.setItem('cachedDevices', this.cachedDevices)
 					callback()
 				})
 				.updateValue(input.hidden)
 
 			inputService.getCharacteristic(Characteristic.ConfiguredName)
-				.on('set', (name, callback) => {
-					this.log.easyDebug(`${this.name} - Setting new input ConfiguredName: from ${input.name} to ${name}`)
-					input.name = name
-					this.storage.setItem('cachedDevices', this.cachedDevices)
+				.on('set', (newName, callback) => {
+					this.log.easyDebug(`Input rename: ${input.name} -> ${newName}`)
+					input.name = newName
 					callback()
 				})
 				.updateValue(input.name)
@@ -123,139 +116,79 @@ class AUDIO_RECEIVER {
 			this.tvService.addLinkedService(inputService)
 		})
 
-		// Set volume service
 		this.speakerService = this.accessory.addService(Service.TelevisionSpeaker)
 			.setCharacteristic(Characteristic.Active, 1)
 			.setCharacteristic(Characteristic.VolumeControlType, Characteristic.VolumeControlType.RELATIVE_WITH_CURRENT)
 
 		this.speakerService.getCharacteristic(Characteristic.Volume)
-			.on('set',stateManager.set.Volume.bind(this))
+			.on('set', stateManager.set.Volume.bind(this))
 			.updateValue(this.state.volume)
 
 		this.speakerService.getCharacteristic(Characteristic.Mute)
-			.on('set',stateManager.set.Mute.bind(this))
+			.on('set', stateManager.set.Mute.bind(this))
 			.updateValue(this.state.mute)
 
 		this.speakerService.getCharacteristic(Characteristic.VolumeSelector)
 			.on('set', stateManager.set.VolumeSelector.bind(this))
 
-		switch(this.volume.type) {
-			case 'bulb':
-				this.addBulbService()
-				break;
+		switch(this.volumeAccessory) {
 			case 'fan':
 				this.addFanService()
-				break;
-			// case 'speaker':
-			// 	this.addSmartSpeakerService()
-			// 	break;
+				break
+			default:
+				this.addBulbService()
 		}
-
 	}
 
 	addBulbService() {
-		this.log.easyDebug(`Adding Volume Bulb Service for ${this.name}`)
-		this.bulbService = this.accessory.addService(Service.Lightbulb, this.volume.name)
+		this.log.easyDebug(`Adding Volume Bulb for ${this.name}`)
+		this.bulbService = this.accessory.addService(Service.Lightbulb, `${this.name} Volume`)
 
 		this.bulbService.getCharacteristic(Characteristic.On)
-			.on('set',stateManager.set.ExternalMute.bind(this))
+			.on('set', stateManager.set.ExternalMute.bind(this))
 			.updateValue(!this.state.mute)
 
 		this.bulbService.getCharacteristic(Characteristic.Brightness)
-			.on('set',stateManager.set.ExternalVolume.bind(this))
+			.on('set', stateManager.set.ExternalVolume.bind(this))
 			.updateValue(this.state.volume)
-
-		this.bulbService.getCharacteristic(Characteristic.ConfiguredName)
-			.on('set', (name, callback) => {
-				this.log.easyDebug(`${this.name} - Setting new Volume Accessory ConfiguredName: from ${this.volume.name} to ${name}`)
-				this.volume.name = name
-				this.storage.setItem('cachedDevices', this.cachedDevices)
-				callback()
-			})
-			.updateValue(this.volume.name)
 	}
 
 	addFanService() {
-		this.log.easyDebug(`Adding Volume Fan Service for ${this.name}`)
-		this.fanService = this.accessory.addService(Service.Fan, this.volume.name)
+		this.log.easyDebug(`Adding Volume Fan for ${this.name}`)
+		this.fanService = this.accessory.addService(Service.Fan, `${this.name} Volume`)
 
 		this.fanService.getCharacteristic(Characteristic.On)
-			.on('set',stateManager.set.ExternalMute.bind(this))
+			.on('set', stateManager.set.ExternalMute.bind(this))
 			.updateValue(!this.state.mute)
 
 		this.fanService.getCharacteristic(Characteristic.RotationSpeed)
-			.on('set',stateManager.set.ExternalVolume.bind(this))
+			.on('set', stateManager.set.ExternalVolume.bind(this))
 			.updateValue(this.state.volume)
-
-		this.fanService.getCharacteristic(Characteristic.ConfiguredName)
-			.on('set', (name, callback) => {
-				this.log.easyDebug(`${this.name} - Setting new Volume Accessory ConfiguredName: from ${this.volume.name} to ${name}`)
-				this.volume.name = name
-				this.storage.setItem('cachedDevices', this.cachedDevices)
-				callback()
-			})
-			.updateValue(this.volume.name)
-	}
-
-	addSmartSpeakerService() {
-
-		this.log.easyDebug(`Adding Volume Smart Speaker Service for ${this.name}`)
-		this.smartSpeakerService = this.accessory.addService(Service.SmartSpeaker, this.volume.name)
-
-		this.smartSpeakerService
-			.setCharacteristic(Characteristic.CurrentMediaState, 2)
-			.setCharacteristic(Characteristic.TargetMediaState, 2)
-			
-		this.smartSpeakerService.getCharacteristic(Characteristic.Mute)
-			.on('set',stateManager.set.ExternalMute.bind(this))
-			.updateValue(this.state.mute)
-		
-		this.smartSpeakerService.getCharacteristic(Characteristic.Volume)
-			.on('set',stateManager.set.ExternalVolume.bind(this))
-			.updateValue(this.state.volume)
-			
-		this.smartSpeakerService.getCharacteristic(Characteristic.ConfiguredName)
-			.on('set', (name, callback) => {
-				this.log.easyDebug(`${this.name} - Setting new Volume Accessory ConfiguredName: from ${this.volume.name} to ${name}`)
-				this.volume.name = name
-				this.storage.setItem('cachedDevices', this.cachedDevices)
-				callback()
-			})
-			.updateValue(this.volume.name)
-
 	}
 
 	async updateState() {
-
 		if (!this.processing) {
 			this.processing = true
-			this.state = await stateManager.getState.bind(this)()
+			this.state = await this.getState()
 
-			this.tvService.getCharacteristic(Characteristic.Active).updateValue(this.state.power)
-			this.tvService.getCharacteristic(Characteristic.ActiveIdentifier).updateValue(this.state.source)
-			this.speakerService.getCharacteristic(Characteristic.Volume).updateValue(this.state.volume)
-			this.speakerService.getCharacteristic(Characteristic.Mute).updateValue(this.state.mute)
+			this.tvService.getCharacteristic(this.api.hap.Characteristic.Active).updateValue(this.state.power)
+			this.tvService.getCharacteristic(this.api.hap.Characteristic.ActiveIdentifier).updateValue(this.state.source)
+			this.speakerService.getCharacteristic(this.api.hap.Characteristic.Volume).updateValue(this.state.volume)
+			this.speakerService.getCharacteristic(this.api.hap.Characteristic.Mute).updateValue(this.state.mute)
 
-			switch(this.volume.type) {
-				case 'bulb':
-					this.bulbService.getCharacteristic(Characteristic.On).updateValue(!this.state.mute)
-					this.bulbService.getCharacteristic(Characteristic.Brightness).updateValue(this.state.volume)
-					break;
-				case 'fan':
-					this.fanService.getCharacteristic(Characteristic.On).updateValue(!this.state.mute)
-					this.fanService.getCharacteristic(Characteristic.RotationSpeed).updateValue(this.state.volume)
-					break;
-				// case 'speaker':
-				// 	this.smartSpeakerService.getCharacteristic(Characteristic.Mute).updateValue(this.state.mute)
-				// 	this.smartSpeakerService.getCharacteristic(Characteristic.Volume).updateValue(this.state.volume)
-				// 	break;
+			if (this.volumeAccessory === 'bulb') {
+				this.bulbService.getCharacteristic(this.api.hap.Characteristic.On).updateValue(!this.state.mute)
+				this.bulbService.getCharacteristic(this.api.hap.Characteristic.Brightness).updateValue(this.state.volume)
+			} else {
+				this.fanService.getCharacteristic(this.api.hap.Characteristic.On).updateValue(!this.state.mute)
+				this.fanService.getCharacteristic(this.api.hap.Characteristic.RotationSpeed).updateValue(this.state.volume)
 			}
+
 			setTimeout(() => {
 				this.processing = false
 			}, 1000)
 		}
 	}
 }
-
 
 module.exports = AUDIO_RECEIVER
--- index.js
+++ index.js
@@ -1,52 +1,42 @@
 const AVR = require('./lib/avr')
-const PLUGIN_NAME = 'homebridge-onkyo-pioneer'
-const PLATFORM_NAME = 'OnkyoPioneer'
+const PLUGIN_NAME = 'homebridge-onkyo-tx8050'
+const PLATFORM_NAME = 'OnkyoTX8050'
 const storage = require('node-persist')
 const path = require('path')
 
 module.exports = (api) => {
-	api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, OnkyoPioneer)
+	api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, OnkyoTX8050)
 }
 
-class OnkyoPioneer {
+class OnkyoTX8050 {
 
 	constructor(log, config, api) {
 		this.api = api
 		this.log = log
 		this.storage = storage
 
-		// this.accessories = []
-		this.avrDevices = []
-		this.PLUGIN_NAME = PLUGIN_NAME
-		this.PLATFORM_NAME = PLATFORM_NAME
-		this.name = config.name || PLATFORM_NAME
-		this.discovery = config.discovery
-		this.receivers = config.receivers || []
-		this.statePollingInterval = config.statePollingInterval
+		this.config = config || {}
+		this.name = this.config.name || PLATFORM_NAME
+		this.debug = this.config.debug || false
+		this.statePollingInterval = this.config.statePollingInterval || 30
 		if (this.statePollingInterval < 3)
 			this.statePollingInterval = 3
-		this.debug = config.debug || false
-		this.persistPath = path.join(this.api.user.persistPath(), '/../onkyo-pioneer-persist')
 
-		
-		// define debug method to output debug logs when enabled in the config
+		this.persistPath = path.join(this.api.user.persistPath(), '/../onkyo-tx8050-persist')
+
+		// Define easyDebug method to output debug logs when enabled
 		this.log.easyDebug = (...content) => {
 			if (this.debug) {
-				this.log(content.reduce((previous, current) => {
-					return previous + ' ' + current
-				}))
-			} else
-				this.log.debug(content.reduce((previous, current) => {
-					return previous + ' ' + current
-				}))
+				this.log(content.reduce((previous, current) => previous + ' ' + current))
+			} else {
+				this.log.debug(content.reduce((previous, current) => previous + ' ' + current))
+			}
 		}
 
 		this.api.on('didFinishLaunching', AVR.init.bind(this))
-
 	}
 
 	configureAccessory(accessory) {
-		this.log.easyDebug(`Found Cached Accessory: ${accessory.displayName} (${accessory.context.deviceId}) `)
-		// this.accessories.push(accessory)
+		this.log.easyDebug(`Found Cached Accessory: ${accessory.displayName} (${accessory.context.deviceId})`)
 	}
 }
